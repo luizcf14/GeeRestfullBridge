9c2d5b4bd4d370fb35b8d488c75e9799402fc735
Luiz Cortinhas <luizcf14@gmail.com>
2021-11-08 17:52:30 -0300

    feat: added S1 and S2

--- main.py
+++ main.py
@@ -17,17 +17,37 @@ class getThumbs:
     def imageToThumb(self,img,imageBands,imageOrder):
         eeMin = 0.03
         eeMax = 0.4
-        eeimg = ee.Image(img).select([1,2,3,4,5,6,'BQA'],['blue','green','red','nir','swir1','swir2','BQA'])
-        if(imageBands == 'ndvi,'):
+        if("LANDSAT" in img):
+            eeimg = ee.Image(img).select([1,2,3,4,5,6,'BQA'],['blue','green','red','nir','swir1','swir2','BQA'])
+        if("S2_SR" in img):
+            eeimg = ee.Image(img).select([1,2,3,8,11,12],['blue','green','red','nir','swir1','swir2'])
+        if("S1_GRD" in img):
+            eeimg = ee.Image(img).select(['VH','VH','VH','VH','VH','VH'],['blue','green','red','nir','swir1','swir2'])
+        if("S2" in img):
+            # print("S2")
+            eeMin = 91
+            eeMax = 3700
+        if(imageBands == 'ndvi,' and "S1_GRD" not in img):
             eeMin = 0.1
             eeMax = 0.9
+            if("S2_SR" in img):
+                eeMin = -0.15
+                eeMax = 0.9
             imageBands = ['NDVI']
             eeimg = eeimg.normalizedDifference(['nir','red']).rename('NDVI')
         else:
             if(imageBands == 'red,green,blue'):
+                if("S2" in img):
+                    # print("S2")
+                    eeMin = 210
+                    eeMax = 2140
                 eeMin = 0.05
                 eeMax = 0.12
             imageBands = imageBands.split(',')
+        if("S1_GRD" in img):
+            # print("S1")
+            eeMin = -29
+            eeMax = -12
         
         visGeomR = ee.Image(0).toByte().paint(self.geeGeometry,255,4)
         visGeomG = ee.Image(0).toByte()
@@ -52,7 +72,10 @@ class getThumbs:
         })
 
         mapid = eeimg.getMapId({'bands': imageBands, 'min': eeMin, 'max': eeMax})
-        return {'order':imageOrder,'thumb' : imageThumb, 'tile' : mapid['tile_fetcher'].url_format, 'imgID':img,'date':ee.Image(img).get('DATE_ACQUIRED').getInfo()}
+        date = ee.Image(img).get('DATE_ACQUIRED').getInfo()
+        if("COPERNICUS" in img):
+            date = datetime.datetime.fromtimestamp(ee.Image(img).get('system:time_start').getInfo()/1000).strftime("%Y-%m-%d")   
+        return {'order':imageOrder,'thumb' : imageThumb, 'tile' : mapid['tile_fetcher'].url_format, 'imgID':img,'date':date}
 
     def on_post(self, req, resp):
         imageName = req.get_param('image',False)
@@ -65,18 +88,20 @@ class getThumbs:
         resp.status = falcon.HTTP_200 
         resp.media = imageThumbLink
 
-    def on_get(self, req, resp):
-        imageName = req.get_param('image',False)
-        polygon = json.loads(req.get_param('polygon',False)  or req.params['polygon'])  #get via GET or POST
-        self.geeGeometry = ee.Geometry.MultiPolygon(polygon['coordinates'],'EPSG:4326',True)
-        self.featureArea = ee.Number(self.geeGeometry.area()).sqrt().getInfo()
-        self.imageToThumb(imageName) 
-        resp.status = falcon.HTTP_200 
-        resp.media = 'quote'
+    # def on_get(self, req, resp):
+    #     imageName = req.get_param('image',False)
+    #     polygon = json.loads(req.get_param('polygon',False)  or req.params['polygon'])  #get via GET or POST
+    #     self.geeGeometry = ee.Geometry.MultiPolygon(polygon['coordinates'],'EPSG:4326',True)
+    #     self.featureArea = ee.Number(self.geeGeometry.area()).sqrt().getInfo()
+    #     self.imageToThumb(imageName) 
+    #     resp.status = falcon.HTTP_200 
+    #     resp.media = 'quote'
 
 class getImageList:
     ee.Initialize()
     landsat8 = ee.ImageCollection("LANDSAT/LC08/C01/T1_TOA")
+    sentinel2 = ee.ImageCollection("COPERNICUS/S2_SR")
+    sentinel1 = ee.ImageCollection("COPERNICUS/S1_GRD")
     EE_TILES = 'https://earthengine.googleapis.com/map/{mapid}/{{z}}/{{x}}/{{y}}?token={token}'
     geeGeometry = ''
     featureArea = 0
@@ -89,8 +114,10 @@ class getImageList:
         if(polygon != ''):
             self.geeGeometry = ee.Geometry.MultiPolygon(polygon['coordinates'],'EPSG:4326',True)
             self.featureArea = ee.Number(self.geeGeometry.area()).sqrt().getInfo()
-            landsatList = self.landsat8.filterDate('2019-01-01','2030-01-01').filterBounds(self.geeGeometry).limit(25,'system:time_end',False).aggregate_array('system:id').getInfo()
-            print(landsatList)
+            landsatListL8 = self.landsat8.filterDate('2019-01-01','2030-01-01').filterBounds(self.geeGeometry).limit(25,'system:time_end',False).aggregate_array('system:id').getInfo()
+            landsatListS2 = self.sentinel2.filterDate('2019-01-01','2030-01-01').filterBounds(self.geeGeometry).limit(25,'system:time_end',False).aggregate_array('system:id').getInfo()
+            landsatListS1 = self.sentinel1.filterDate('2019-01-01','2030-01-01').filterBounds(self.geeGeometry).limit(25,'system:time_end',False).aggregate_array('system:id').getInfo()
+            landsatList = [*landsatListL8,*landsatListS2,*landsatListS1]
             #landsatList = list(map(self.imageToThumb,landsatList))
             resp.media = landsatList
         else:
